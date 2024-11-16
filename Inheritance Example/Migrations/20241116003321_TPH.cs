a89ab736a6e7838afb28a4606fb392fd33856d4d
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-11-16 07:26:25 +0200

    Inheritance Example

---
+++ Inheritance Example/Migrations/20241116003321_TPH.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inheritance_Example.Migrations
+{
+    /// <inheritdoc />
+    public partial class TPH : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(21)", maxLength: 21, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    NumberOfHours = table.Column<int>(type: "int", nullable: true),
+                    HourRate = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Inheritance Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using Inheritance_Example.Contexts;
+using Inheritance_Example.Models;
+
+namespace Inheritance_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TPC
+            // TPH
+            // TPCC
+
+            using AppDbContext context = new AppDbContext();
+
+            FullTimeEmployee fullTimeEmployee = new FullTimeEmployee
+            {
+                Name = "John Doe2",
+                Email = "sdsd",
+                Address = "sdsd",
+                Salary = 10000
+
+            };
+
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee
+            {
+                Name = "Jane Doe2",
+                Email = "sdsd",
+                Address = "sdsd",
+                HourRate = 100,
+                NumberOfHours = 20
+            };
+
+            context.FullTimeEmployees.Add(fullTimeEmployee);
+            context.PartTimeEmployees.Add(partTimeEmployee);
+
+            var result = context.Employees.OfType<FullTimeEmployee>();
+
+            foreach (var resultItem in result)
+            {
+                Console.WriteLine(resultItem);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
