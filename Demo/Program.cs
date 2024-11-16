db6e8be1a8a09e102b4a54aff19972ac2d35c407
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-11-16 19:12:55 +0200

    Loading of Navigational Properties

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace Demo
+﻿using Demo.Contexts;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // EF Core does not load the navigational properties by default
+
+            using AppDbContext context = new AppDbContext();
+
+            var result = context.Employees.FirstOrDefault(e => e.Id == 2);
+
+            Console.WriteLine(result?.Id ?? 0);
+            Console.WriteLine(result?.Name ?? "NA");
+            Console.WriteLine(result?.Salary ?? 0);
+            Console.WriteLine(result?.Address ?? "NA");
+            Console.WriteLine(result?.Department.Name ?? "NA");
+
+            var department = context.Departments.FirstOrDefault(e => e.Id == 1);
+
+            Console.WriteLine(department?.Name ?? "NA");
+
+            foreach (var emp in department.Employees)
+            {
+                Console.WriteLine(emp.Name);
+            }
+
+            //Loading of the navigational properties can be done using 3 ways 
+            //1. Eager Loading
+            //2. Explicit Loading
+            //3. Lazy Loading
 
         }
     }
--- Inheritance Example/Program.cs
+++ Inheritance Example/Program.cs
@@ -11,37 +11,37 @@ namespace Inheritance_Example
             // TPH
             // TPCC
 
-            using AppDbContext context = new AppDbContext();
+            //    using AppDbContext context = new AppDbContext();
 
-            FullTimeEmployee fullTimeEmployee = new FullTimeEmployee
-            {
-                Name = "John Doe2",
-                Email = "sdsd",
-                Address = "sdsd",
-                Salary = 10000
+            //    FullTimeEmployee fullTimeEmployee = new FullTimeEmployee
+            //    {
+            //        Name = "John Doe2",
+            //        Email = "sdsd",
+            //        Address = "sdsd",
+            //        Salary = 10000
 
-            };
+            //    };
 
-            PartTimeEmployee partTimeEmployee = new PartTimeEmployee
-            {
-                Name = "Jane Doe2",
-                Email = "sdsd",
-                Address = "sdsd",
-                HourRate = 100,
-                NumberOfHours = 20
-            };
+            //    PartTimeEmployee partTimeEmployee = new PartTimeEmployee
+            //    {
+            //        Name = "Jane Doe2",
+            //        Email = "sdsd",
+            //        Address = "sdsd",
+            //        HourRate = 100,
+            //        NumberOfHours = 20
+            //    };
 
-            context.FullTimeEmployees.Add(fullTimeEmployee);
-            context.PartTimeEmployees.Add(partTimeEmployee);
+            //    context.FullTimeEmployees.Add(fullTimeEmployee);
+            //    context.PartTimeEmployees.Add(partTimeEmployee);
 
-            var result = context.Employees.OfType<FullTimeEmployee>();
+            //    var result = context.Employees.OfType<FullTimeEmployee>();
 
-            foreach (var resultItem in result)
-            {
-                Console.WriteLine(resultItem);
-            }
+            //    foreach (var resultItem in result)
+            //    {
+            //        Console.WriteLine(resultItem);
+            //    }
 
-            context.SaveChanges();
+            //    context.SaveChanges();
         }
     }
 }
