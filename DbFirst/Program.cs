2213ba3b9461ca5002f2eef0ef67c97b92b85f2b
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-12-09 23:45:35 +0200

    Run SQL Query

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System;
 using Microsoft.EntityFrameworkCore;
+using DbFirst.Contexts;
 
 namespace DbFirst
 {
@@ -13,7 +14,7 @@ namespace DbFirst
 
             //Scaffold - DbContext "Server=MOHAMED_PC;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer - Context AppDbContext - OutputDir Data\Models
 
-            using AppDbContext context = new AppDbContext(); 
+            NorthwindContext context = new NorthwindContext();
 
             var result = context.Categories.ToList();
 
@@ -22,6 +23,46 @@ namespace DbFirst
                 Console.WriteLine(item.CategoryName);
             }
 
+            // Run SQL Query
+            // 1. Select Statement : FromSqlRaw(), FromSqlInterpolated()
+            
+            result = context.Categories.FromSqlRaw("SELECT * FROM Categories").ToList();
+
+            result = context.Categories.FromSqlInterpolated($"SELECT * FROM Categories WHERE CategoryID = {1}").ToList();
+
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+
+            // 2. DML Statement [Update - Insert - Delete] : ExecuteSqlRaw(), ExecuteSqlInterpolated()
+
+            context.Categories.FromSqlRaw("UPDATE Categories SET CategoryName = 'New Name' WHERE CategoryID = 1");
+
+            context.Categories.FromSqlInterpolated($"UPDATE Categories SET CategoryName = 'New Name' WHERE CategoryID = {1}");
+
+            // 3. Stored Procedures : FromSqlRaw(), FromSqlInterpolated()
+
+            result = context.Categories.FromSqlRaw("EXEC dbo.GetCategories").ToList();
+
+            result = context.Categories.FromSqlInterpolated($"EXEC dbo.GetCategories").ToList();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+
+            // 4. Functions : FromSqlRaw(), FromSqlInterpolated()
+
+            result = context.Categories.FromSqlRaw("SELECT * FROM dbo.GetCategories()").ToList();
+
+            result = context.Categories.FromSqlInterpolated($"SELECT * FROM dbo.GetCategories()").ToList();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
         }
     }
 }
